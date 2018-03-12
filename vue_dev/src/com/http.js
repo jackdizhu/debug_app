89fc2cbd022272951f80ccd5cbc824cbdf8487a7
jackdizhu <376365334@qq.com>
2018-03-12 17:47:05 +0800

    前端 错误处理

--- vue_dev/src/com/http.js
+++ vue_dev/src/com/http.js
@@ -1,11 +1,13 @@
 import axios from 'axios'
 import https from 'https'
+import storage from '../com/com.js'
+
 axios.defaults.timeout = 1000 * 60 * 60
 // axios.defaults.baseURL = 'http://127.0.0.1:8000/mock/5a522f2eb9574d08787bf76a/app1'
 axios.defaults.baseURL = 'http://127.0.0.1:3000'
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8'
 // token 验证
-axios.defaults.headers.post['authorization'] = sessionStorage.getItem('token') || ''
+axios.defaults.headers['authorization'] = storage.getItem('token') || ''
 // axios.defaults.withCredentials = true // 带cookie 请求
 axios.defaults.httpsAgent = new https.Agent({
   rejectUnauthorized: false
